54735dd765db67718f1598ccfe13e38bdba6814d
Mahesh Heda <mahesh.heda@outlook.com>
2025-04-09 18:57:06 +0530

    add git ignore

--- NewMarkAssignment/obj/Debug/net6.0/NewMarkAssignment.AssemblyInfo.cs
+++ NewMarkAssignment/obj/Debug/net6.0/NewMarkAssignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewMarkAssignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e90efd57d9dd57bab91d395114dabea5e52153e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewMarkAssignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewMarkAssignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
